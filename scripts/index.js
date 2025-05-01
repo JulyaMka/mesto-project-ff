04db7693d9d28dc9c33cf1231c4f6818f3985900
Maksimenko Julia <julya.mka@yandex.ru>
2025-05-01 17:14:17 +0500

    Первый коммит

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
 // @todo: Функция создания карточки
+function addCard(cardList) {
+  // @todo: Темплейт карточки
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  // @todo: DOM узлы
+  cardElement.querySelector('.card__image').src = cardList.link;
+  cardElement.querySelector('.card__title').textContent = cardList.name;
+  cardElement.setAttribute('alt', cardList.name);
 
-// @todo: Функция удаления карточки
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+
+  return cardElement;
+  // @todo: Функция удаления карточки
+  function deleteCard() {
+    const cards = this.parentElement;
+    cards.remove();
+  }  
+} 
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+initialCards.forEach((item) =>{
+  const cardElement = addCard(item);
+  placesList.append(cardElement);
+});
