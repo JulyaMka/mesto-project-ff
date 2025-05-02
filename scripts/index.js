6150c558156d04e34f842ca2689318ccf6862e21
Maksimenko Julia <julya.mka@yandex.ru>
2025-05-02 14:05:55 +0500

    Исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,28 @@
 // @todo: Функция создания карточки
-function addCard(cardList) {
+function createCard(cardData) {
   // @todo: Темплейт карточки
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   // @todo: DOM узлы
-  cardElement.querySelector('.card__image').src = cardList.link;
-  cardElement.querySelector('.card__title').textContent = cardList.name;
-  cardElement.setAttribute('alt', cardList.name);
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.setAttribute('alt', cardData.name);
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteCard);
+  deleteButton.addEventListener("click", ()=> deleteCard(cardElement));
 
   return cardElement;
-  // @todo: Функция удаления карточки
-  function deleteCard() {
-    const cards = this.parentElement;
-    cards.remove();
-  }  
+  
 } 
+
+// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}  
 
 // @todo: Вывести карточки на страницу
 const placesList = document.querySelector('.places__list');
 initialCards.forEach((item) =>{
-  const cardElement = addCard(item);
+  const cardElement = createCard(item, deleteCard);
   placesList.append(cardElement);
 });
